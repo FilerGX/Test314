de1d9c87d986fdf9d5e8ba869a1222917774ef4f
ygx <983451599@qq.com>
2022-03-14 16:34:06 +0800

    First Commit

---
+++ src/main/java/com/example/test314/Test314Application.java
@@ -0,0 +1,13 @@
+package com.example.test314;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test314Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Test314Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/test314/Test314ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test314;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test314ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
